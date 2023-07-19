5e817f9d97ad0df3d0838d3d36a8d67b9d8f68ce
“ohiseul” <dltmf3140@gmail.com>
2023-07-19 14:52:21 +0900

    연습문제

--- day1/exercise.py
+++ day1/exercise.py
@@ -1,3 +1,4 @@
+""""
 a = "Dog is love"
 
 print(a[7:11])
@@ -13,3 +14,11 @@ print(c.replace("-",""))
 a = "hello"
 b = "python"
 print(a+" ! "+b)
+"""
+a = ["red","orange","yellow","green","blue","purple","black","white"]
+
+a.remove("green")
+print(a)
+
+a.insert(1,"pink")
+print(a)
