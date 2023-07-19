da7fcea6bbff1279d7e6e98f343849dc1b893bce
“ohiseul” <dltmf3140@gmail.com>
2023-07-19 13:46:10 +0900

    연습문제

---
+++ day1/exercise.py
@@ -0,0 +1,15 @@
+a = "Dog is love"
+
+print(a[7:11])
+print(a.replace("Dog","Cat"))
+
+b = "lalalalalalalalalalala"
+print(b.count("l"),b.count("a"))
+
+
+c = "010-1234-5678"
+print(c.replace("-",""))
+
+a = "hello"
+b = "python"
+print(a+" ! "+b)
