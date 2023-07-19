21e32136298bc7ae62659f4cd4981b5b43c5957b
“ohiseul” <dltmf3140@gmail.com>
2023-07-19 10:49:20 +0900

    중간

---
+++ day1/main.py
@@ -0,0 +1,26 @@
+print('hello')
+
+a=10
+b=5
+c="안녕하세요"
+
+
+
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a**b)
+
+a += 50
+print(a)
+a -= 20
+print(a)
+
+
+print('*' * 10)
+
+happy_face ='🥰'
+print(happy_face)
