d0ae8e6fdae500530890969d78055d5ddac214f5
“ohiseul” <dltmf3140@gmail.com>
2023-07-21 17:52:39 +0900

    당고 끝

--- day2/Django/products/urls.py
+++ day2/Django/products/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from django.views.generic.list import ListView
+# from .views import BookDetailView, BookListView
+from .views import book_list, book_detail
 
-from .views import BookDetailView, BookListView
 
 urlpatterns = [
-    path("books", BookListView.as_view(), name="book-list"),
-    path("books/<int:pk>", BookDetailView.as_view(), name="book-detail")
+    path("books", book_list, name="book-list"),
+    path("books/<int:pk>", book_detail, name="book-detail")
 ]
--- day2/Django/products/views.py
+++ day2/Django/products/views.py
@@ -1,15 +1,38 @@
-from django.views.generic.detail import DetailView
-from django.views.generic.list import ListView
-from .models import Author, Book
+# from django.views.generic.detail import DetailView
+# from django.views.generic.list import ListView
+# from .models import Author, Book
 
 
-class BookListView(ListView):
-    model = Book
-    template_name = "book_list.html"
+# class BookListView(ListView):
+#     model = Book
+#     template_name = "book_list.html"
 
 
-class BookDetailView(DetailView):
-    model = Book
-    template_name = "book_detail.html"
+# class BookDetailView(DetailView):
+#     model = Book
+#     template_name = "book_detail.html"
 
-    
+from django.http import JsonResponse
+from .models import Book, Author
+
+def book_list(request):
+    print('Request를 받았다')
+    books = Book.objects.all()  # Book object를 다 가져온다
+    data = {"books": list(books.values())}
+    response = JsonResponse(data, safe=False, json_dumps_params={'ensure_ascii': False})
+    return response
+
+def book_detail(request, pk):
+    book = Book.objects.get(pk=pk)  # 특정 Book object를 가져온다
+    data = {
+        "book": {
+            "author": book.author.name,
+            "name": book.name,
+            "description": book.description,
+            "shipping_cost": book.shipping_cost,
+            "quantity": book.quantity
+        }
+    }
+    response = JsonResponse(data, safe=False, json_dumps_params={'ensure_ascii': False})
+    return response
+    # 원래는 없는 경우에 대비해서 try / except Book.DoesnotExist  등 처리를 해줘야 함
